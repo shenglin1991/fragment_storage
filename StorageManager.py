00c55ba903311529e1f375ff3047a16e6d278d37
shenglin1991 <linsheng9090@hotmail.com>
2016-07-26 11:55:01 +0200

    add is_db method for storage manager

--- StorageManager.py
+++ StorageManager.py
@@ -10,6 +10,12 @@ class StorageManager(object):
         self.storages = []
         self._default_storage = None
 
+    def is_db(self, storage_name):
+        storage = self.get_storage_instance(storage_name)
+        if not storage:
+            raise TypeError("Storage doesn't exist")
+        return storage.get('type') == 'db'
+
     def get_databases(self):
         return [{'name': storage.get('name'),
                  'type': 'db'}
@@ -35,11 +41,10 @@ class StorageManager(object):
         return storage.get('connection'), storage.get(io_type)
 
     def set_storage_handler(self, storage_name, handler, io_type='write'):
-        for storage in self.storages:
-            if storage_name == storage.get('name'):
-                storage.update({io_type: handler})
-                return True
-        return False
+        storage = self.get_storage_instance(storage_name)
+        if not storage:
+            raise TypeError("Storage doesn't exist")
+        storage.update({io_type: handler})
 
     def add_storage(self, conn, name, storage_type='db', write_handler=None, read_handler=None):
         self.storages.append({
@@ -56,11 +61,8 @@ class StorageManager(object):
         return self._default_storage
 
     def set_default_storage(self, storage_name):
-        for storage in self.storages:
-            if storage.get('name') == storage_name:
-                self._default_storage = storage
-                return
-        self._default_storage = self.get_default_storage()
+        storage = self.get_storage_instance(storage_name)
+        self._default_storage = storage if storage else self.get_default_storage()
 
     def write(self, storage_name, content, placement=None):
         storage_conn, writer = self.get_storage_handler(storage_name, 'write')
