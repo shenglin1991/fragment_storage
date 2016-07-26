f5256d3b41c41533bc3f5f03117814db07c35eb9
shenglin1991 <linsheng9090@hotmail.com>
2016-07-26 11:55:31 +0200

    adapt ceph read into store manager

--- store.py
+++ store.py
@@ -20,30 +20,46 @@ def get_test_storage_manager(root_db):
     from storages.file_storage import file_storage
     from storages.mongo_db import mongo_conn, mongo_writer, mongo_reader
 
-    fs_hot = file_storage({'name': '/mnt/hotsemantic/'})
-    fs_warm = file_storage({'name': '/mnt/warmsemantic/'})
-    fs_cold = file_storage({'name': '/mnt/coldsemantic/'})
-    db2 = mongo_conn({
+    fs_hot = file_storage({'name': '/mnt/hotsemantic/',
+                           'pathname': '/mnt/hotsemantic/'})
+    fs_warm = file_storage({'name': '/mnt/warmsemantic/',
+                            'pathname': '/mnt/warmsemantic/'})
+    fs_cold = file_storage({'name': '/mnt/coldsemantic/',
+                            'pathname': '/mnt/coldsemantic/'})
+    mongo_db2 = mongo_conn({
         '__DB_ADDR__': 'localhost:27027',
     })
     storage_manager = StorageManager()
 
-    storage_manager.add_database(root_db, 'mongo_db', db_type='noSQL/document',
-                                 write_handler=mongo_writer,
-                                 read_handler=mongo_reader)
-    storage_manager.add_database(db2, 'mongo_db2', db_type='noSQL/document',
-                                 write_handler=mongo_writer,
-                                 read_handler=mongo_reader)
+    storage_manager.add_storage(root_db,
+                                name='mongo_db',
+                                write_handler=mongo_writer,
+                                read_handler=mongo_reader
+                                )
+    storage_manager.add_storage(mongo_db2,
+                                name='mongo_db2',
+                                write_handler=mongo_writer,
+                                read_handler=mongo_reader)
+
+    storage_manager.add_storage(fs_hot,
+                                name='ceph_hot',
+                                storage_type='fs',
+                                write_handler=fs_hot.write,
+                                read_handler=fs_hot.read)
+    storage_manager.add_storage(fs_warm,
+                                name='ceph_warm',
+                                storage_type='fs',
+                                write_handler=fs_warm.write,
+                                read_handler=fs_warm.read)
+    storage_manager.add_storage(fs_cold,
+                                name='ceph_cold',
+                                storage_type='fs',
+                                write_handler=fs_cold.write,
+                                read_handler=fs_cold.read)
+
     storage_manager.set_default_storage('mongo_db2')
-    storage_manager.add_filesystem(fs_hot, 'ceph_hot',
-                                   write_handler=fs_hot.write,
-                                   read_handler=fs_hot.read)
-    storage_manager.add_filesystem(fs_warm, 'ceph_warm',
-                                   write_handler=fs_warm.write,
-                                   read_handler=fs_warm.read)
-    storage_manager.add_filesystem(fs_cold, 'ceph_cold',
-                                   write_handler=fs_cold.write,
-                                   read_handler=fs_cold.read)
+    print 'Pool Databases:', storage_manager.get_databases()
+    print 'Pool File systems:', storage_manager.get_filesystems()
 
     return storage_manager
 
@@ -57,28 +73,27 @@ class Store(object):
         self.storage_manager = storage_manager
         self.root_db = root_db
 
-    def write_field(self, root_db, field, content):
+    def write_field(self, field, content):
         """
         first look for {field: storage} mapping storage set by user.
         if {field: storage} mapping not found, try to find {type: storage} mapping set by default.
         if {type: storage} mapping not found, use default database.
-        :param root_db:
         :param field:
         :param content:
         :return:
         """
         if is_multipart(content):
             # deal with multiple part by storing them and keeping only their address
-            value = [self.write_field(root_db, content.get('name', 'multipart'), part) for part in content['value']]
+            value = [self.write_field(content.get('name', 'multipart'), part) for part in content['value']]
 
             # update 'value' as collection of stored parts' address; 'type' as list
             content.update({'value': value,
                             'type': list.__name__})
 
         # look for place to store the field
-        storage = ((root_db.field_to_storage.find_one({'field': field}) or {}).get('storage') or
-                   (root_db.type_to_storage.find_one({'type': content.get('type')}) or {})
-                   .get('storage') or self.storage_manager.get_default_storage('db'))
+        storage = ((self.root_db.field_to_storage.find_one({'field': field}) or {}).get('storage') or
+                   (self.root_db.type_to_storage.find_one({'type': content.get('type')}) or {}).get('storage') or
+                   self.storage_manager.get_default_storage())
         if not storage:
             raise ValueError("No storage available!")
 
@@ -100,7 +115,7 @@ class Store(object):
         print 'INFO: Write original object into target storage, storing location information in target table'
         target_object = {}
         for key, value in original_object.iteritems():
-            location = self.write_field(self.root_db, key, value)
+            location = self.write_field(key, value)
             target_object.update({key: location})
 
         return (self.storage_manager.write(target_storage, target_object, placement=target_table)
@@ -128,7 +143,7 @@ class Store(object):
         if _id:
             filtre.update({'_id': ObjectId(_id)})
 
-        print 'INFO: looking for object to store in database'
+        print 'INFO: Looking for object to store in database'
         original_object = self.root_db[original_table].find_one(filtre, {'_id': 0})
         if not original_object:
             raise ValueError("ERROR: Object not found, check the condition or maybe it's already been proceeded")
@@ -136,22 +151,25 @@ class Store(object):
         # store object found
         return self.write_object(original_object, target_storage, target_table)
 
-    def read_multipart(self, part):
+    def entire_read(self, part):
         if isinstance(part, str) or isinstance(part, unicode):
             part_info = part
         elif isinstance(part, dict) and part.get('address'):
             part_info = self.storage_manager.read(part.get('storage'),
                                                   part.get('collection'),
                                                   {'address': part.get('address')})
+            if not self.storage_manager.is_db(part.get('storage')):
+                part_info = json.loads(part_info)
+
             for subpart in part_info:
-                part_info.update({subpart: self.read_multipart(part_info.get(subpart))})
+                part_info.update({subpart: self.entire_read(part_info.get(subpart))})
         elif isinstance(part, list):
-            part_info = [self.read_multipart(subpart) for subpart in part]
+            part_info = [self.entire_read(subpart) for subpart in part]
         else:
             raise TypeError('TODO: handle with unrecognized type ' + type(part).__name__)
         return part_info
 
-    def read_subpart(self, part, projection):
+    def selective_read(self, part, projection):
         print 'READ INFO: split projection'
         projection = projection.split('.', 1)
 
@@ -175,6 +193,8 @@ class Store(object):
         next_part = self.storage_manager.read(actual_part.get('storage'),
                                               actual_part.get('collection'),
                                               {'address': actual_part.get('address')})
+        if not self.storage_manager.is_db(actual_part.get('storage')):
+            next_part = json.loads(next_part)
         if not next_part:
             raise ValueError('Invalid path')
 
@@ -182,11 +202,11 @@ class Store(object):
             next_part = next_part.get('value')
 
         if len(projection) == 1:
-            return self.read_multipart(next_part)
+            return self.entire_read(next_part)
 
         else:
             next_projection = projection[1]
-            return self.read_subpart(next_part, next_projection)
+            return self.selective_read(next_part, next_projection)
 
     def read_handler(self, msg):
         """
@@ -214,14 +234,16 @@ class Store(object):
 
         if projection is None:
             # If no demand for projection, get object with its entire content
+            print 'READ INFO: Read entire object without projection'
             for part in read_object:
-                read_object.update({part: self.read_multipart(read_object.get(part))})
+                read_object.update({part: self.entire_read(read_object.get(part))})
             return _id, read_object
         else:
             projected_object = {}
             # If there exists projection, look up for only needed part
+            print 'READ INFO: Read object with projection'
             for key in projection.split(','):
-                projected_object.update({key: self.read_subpart(read_object.get(key.split('.')[0]), key)})
+                projected_object.update({key: self.selective_read(read_object.get(key.split('.')[0]), key)})
             return _id, projected_object
 
     def run(self, broker):
@@ -238,12 +260,14 @@ class Store(object):
                 print 'INFO: Receive from "write" channel: {}'.format(msg)
                 try:
                     print self.write_handler(json.loads(msg['data']))
-                except:
-                    print 'ERROR: Fail to write object'
+                except ValueError as err:
+                    print 'ERROR: ', err
+                except Exception as err:
+                    print 'ERROR: Fail to write object', err.__doc__
 
             elif msg['channel'] == 'read':
                 print 'INFO: Receive from "read" channel: {}'.format(msg)
-                print 'INFO: Reading from cloud'
+                print 'INFO: Reading from cloud'                                            
                 _id, result = self.read_handler(json.loads(msg['data']))
                 print 'INFO: Returning read result'
                 broker.publish('read_result', json.dumps({
@@ -255,12 +279,12 @@ class Store(object):
 
 def main(storage_manager=None):
     db_ = mongo_conn()
-    redis_ = redis_conn()
+    broker = redis_conn()
     if storage_manager is None:
         storage_manager = get_test_storage_manager(root_db=db_)
 
     store = Store(storage_manager, root_db=db_)
-    store.run(redis_)
+    store.run(broker)
 
 
 if __name__ == '__main__':
